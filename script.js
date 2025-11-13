7e1310817fef392428a66222ddac193823e43de4
Utkarsh102002 <128974569+Utkarsh102002@users.noreply.github.com>
2025-11-13 15:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-//your JS code here. If required.
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+
+let currentStep = 1;
+
+// NEXT BUTTON
+nextBtn.addEventListener("click", () => {
+    currentStep++;
+
+    if (currentStep > circles.length) {
+        currentStep = circles.length;
+    }
+
+    update();
+});
+
+// PREVIOUS BUTTON
+prevBtn.addEventListener("click", () => {
+    currentStep--;
+
+    if (currentStep < 1) {
+        currentStep = 1;
+    }
+
+    update();
+});
+
+function update() {
+    // Activate circles
+    circles.forEach((circle, index) => {
+        if (index < currentStep) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    });
+
+    // Update progress line
+    const activeCircles = document.querySelectorAll(".active").length;
+    progress.style.width = ((activeCircles - 1) / (circles.length - 1)) * 100 + "%";
+
+    // Disable / Enable buttons
+    prevBtn.disabled = currentStep === 1;
+    nextBtn.disabled = currentStep === circles.length;
+}
